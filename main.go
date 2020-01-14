741c8e59e76fc1e81036e1ee4ff29e20068e8088
julienboulet <julien.boulet@abtasty.com>
2020-01-14 11:56:44 +0100

    use init func

--- main.go
+++ main.go
@@ -1,21 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/schema"
 	"github.com/jboulet/fizzbuzz-go/app"
-	"github.com/jboulet/fizzbuzz-go/config"
 	"github.com/jboulet/fizzbuzz-go/db"
+	"github.com/kelseyhightower/envconfig"
 	"log"
 	"net/http"
 )
 
+type Specification struct {
+	BD_HOST     string `default:"localhost"`
+	DB_PORT     string `default:"5432"`
+	DB_USERNAME string `default:"postgres"`
+	DB_PASSWORD string `default:"postgres"`
+	DB_NAME     string `default:"postgres"`
+	SERVER_PORT string `default:"8080"`
+	BS_SERVER   string `default:"localhost:9095,localhost:9093,localhost:9094"`
+	TOPIC       string `default:"myTopic"`
+}
+
+var s Specification
+
+func init() {
+	err := envconfig.Process("", &s)
+	if err != nil {
+		log.Fatal("Env variables loading failed: ", err.Error())
+	}
+
+	log.Printf("Specification : ", s)
+
+}
+
 func main() {
-	config.UpdateEnv()
 
-	s := config.GetSpecification()
-
-	database, err := db.CreateDatabase()
+	database, err := db.CreateDatabase(fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", s.BD_HOST, s.DB_PORT, s.DB_USERNAME, s.DB_PASSWORD, s.DB_NAME))
 	if err != nil {
 		log.Fatal("Database connection failed: ", err.Error())
 	}
